587ac0de6ec5d2ee1cbea419d5e4fe438368bfeb
adrhh <adrianhoryn@gmail.com>
2017-10-25 10:17:48 +0200

    Update struct_to_file.c

--- exercises/struct_to_file.c
+++ exercises/struct_to_file.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct student {
+typedef struct student
+{
 	char nazwisko[30];
 	char imie[20];
 	int numer;
@@ -20,10 +21,12 @@ double minimum(char*);
 int usun(char*);
 void popraw(char*);
 
-int main() {
+int main() 
+{
 	char wybor;
 	
-	while (1) {
+	while (1)
+	{
 		printf("\nd - Dodaj\n"
 			"w - Wydruk\n"
 			"u - usun\n"
@@ -35,36 +38,40 @@ int main() {
 		scanf(" %c", &wybor);
 		switch (wybor)
 		{
-			case 'd': {
+			case 'd': 
+				{
 				dodaj("plik.txt");
 				break;
-			}
-			case 'w':{
+				}
+			case 'w':
+				{
 				wydruk("plik.txt");
 				break;
-			}
-			case 'u': {
+				}
+			case 'u': 
+				{
 				printf("\nUsunieto %d Studentow\n", usun("plik.txt"));
 				break;
-			}
-			case 'm': {
+				}
+			case 'm': 
+				{
 				int ilosc=0,i=0;
 				printf("1");
 				Student *studenci = najlepsi("plik.txt", &ilosc);
 				for (i = 0; i < ilosc; i++)
-				 {
 					drukuj(*(studenci + i));
-				}
 				break;
-			}
-			case 'p': {
+				}
+			case 'p': 
+				{
 				popraw("plik.txt");
 				break;
-			}
-			case 'q': {
+				}
+			case 'q':
+				{
 				return 0;
 				break;
-			}
+				}
 	
 			default:
 				printf("\nNiepoprawny wybor");
@@ -106,14 +113,18 @@ void dodaj(char* nazwa) {
 	fclose(plik);
 
 }
-struct student srednia(struct student s) {
+
+struct student srednia(struct student s)
+{
 	int i;
 	double sr=0;
 	for (i = 0; i < 5; i++)sr += s.oceny[i];
 	s.srednia=sr / 5.0;
 	return s;
 }
-void drukuj(struct student s) {
+
+void drukuj(struct student s)
+{
 	int i;
 	printf("\nDane:\n\tImie:\t\t %s"
 		"\n\tNazwisko:\t %s \n\tNumer:\t\t %d \n\tOceny: ",s.imie,s.nazwisko,s.numer);
@@ -121,35 +132,37 @@ void drukuj(struct student s) {
 	for (i = 0; i < 5; i++) printf("\n\t\t[%d]%.2lf",i, s.oceny[i]);
 	printf("\n\tSrednia: \t%0.2lf", s.srednia);
 }
-void wydruk(char* nazwa) {
+void wydruk(char* nazwa) 
+{
 	Student s;
 	FILE *plik = fopen(nazwa, "r");
-	if (plik == NULL) {
+	if (plik == NULL) 
 		printf("Nie udalo sie otworzyc pliku"); exit(0);
-	}
-	while (fread(&s, sizeof(Student), 1, plik)) {
+	while (fread(&s, sizeof(Student), 1, plik)) 
 		drukuj(s);
-	}
 	fclose(plik);
 
 }
-double maksimum(char * nazwa) {
+double maksimum(char * nazwa) 
+{
 	Student s;
 	FILE *plik = fopen(nazwa, "r");
 	double maxSrednia = -1;
-	while (fread(&s, sizeof(Student), 1, plik)) {
+	while (fread(&s, sizeof(Student), 1, plik)) 
 		if (s.srednia > maxSrednia)maxSrednia = s.srednia;
-	}
 	fclose(plik);
 	return maxSrednia;
 }
-struct student* najlepsi(char * nazwa, int* ilosc) {
+struct student* najlepsi(char * nazwa, int* ilosc) 
+{
 	double maxSrednia = maksimum("plik.txt");
 	Student *studenci= malloc(sizeof(Student)) , s;
 	FILE *plik=fopen(nazwa,"rb");
 	int i = 0;
-	while (fread(&s, sizeof(Student), 1, plik)) {
-		if (s.srednia == maxSrednia) {
+	while (fread(&s, sizeof(Student), 1, plik)) 
+	{
+		if (s.srednia == maxSrednia) 
+		{
 			i++;
 			studenci = realloc(studenci, i * sizeof(Student));
 			*(studenci + i-1) = s;
@@ -160,17 +173,21 @@ struct student* najlepsi(char * nazwa, int* ilosc) {
 	return studenci;
 
 }
-double minimum(char* nazwa) {
+
+double minimum(char* nazwa) 
+{
 	Student s;
 	double minSrednia=-1;
 	FILE *plik = fopen(nazwa, "r+b");
-	while (fread(&s, sizeof(Student), 1, plik)) {
+	while (fread(&s, sizeof(Student), 1, plik))
+	{
 		if (s.srednia < minSrednia) minSrednia = s.srednia;
 		else if (minSrednia < 0)minSrednia = s.srednia;
 	}
 	fclose(plik);
 	return minSrednia;
 }
+
 int usun(char* nazwa) { //zwarca ile usunieto
 	int deletedStudents=0;
 	Student s;
@@ -178,28 +195,30 @@ int usun(char* nazwa) { //zwarca ile usunieto
 	FILE *plik=fopen(nazwa,"r+b");
 	FILE *tmpFile = fopen("tmp.txt", "wb");
 
-	while (fread(&s, sizeof(Student), 1, plik)) {
+	while (fread(&s, sizeof(Student), 1, plik)) 
+	{
 		if (s.srednia != minSrednia) fwrite(&s, sizeof(Student), 1, tmpFile);
 		else deletedStudents++;
 	}
 	fclose(plik); fclose(tmpFile);
-	if (remove(nazwa) == 0) {
+	if (remove(nazwa) == 0) 
 		if (rename("tmp.txt", nazwa) != 0) printf("\nBrak dostepu do pliku tmp.txt");
-	}
 	else
-	{
 		printf("\nBrak dostepu do pliku %s", nazwa);
-	}
+
 	return deletedStudents;
 }
-void popraw(char* nazwa) {
+void popraw(char* nazwa)
+{
 	char imie[20], nazwisko[30];
 	int numerOceny,nowaOcena,index=-1;
 	char znaleziono=0;
 	Student s;
 	FILE *plik=fopen(nazwa,"r+b");
-	if (plik == NULL) {
-		printf("Nie udalo sie otworzyc pliku"); exit(0);
+	if (plik == NULL) 
+	{
+		printf("Nie udalo sie otworzyc pliku");
+		exit(0);
 	}
 	printf("Podaj Imie: \t");
 	scanf(" %s", &imie);
@@ -215,7 +234,8 @@ void popraw(char* nazwa) {
 			drukuj(s);
 			printf("\nKtora ocene poprawic?\n");
 			scanf("%d", &numerOceny);
-			if (numerOceny < 0 || numerOceny >= 5) {
+			if (numerOceny < 0 || numerOceny >= 5) 
+			{
 				do
 				{
 					printf("\nNie ma oceny z takim indeksem. Wpisz jeszcze raz.\n");
@@ -232,5 +252,6 @@ void popraw(char* nazwa) {
 			fclose(plik);
 		}
 	}
-	if (znaleziono == 0) printf("\nNie znaleziono ucznia");
+	if (znaleziono == 0) 
+		printf("\nNie znaleziono ucznia");
 }
